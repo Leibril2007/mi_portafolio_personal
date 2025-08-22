421ba2b8fd6af0cdd2224aa146e22ca1ac36c04e
Leibril2007 <ariabi.sr@gmail.com>
2025-08-21 19:13:42 -0600

    se agregan proyectos

---
+++ components/projects/projects.js
@@ -0,0 +1,23 @@
+import { cadaProject } from "../../modules/itemProject.js";
+import { dataProyectos } from "../../data/data.js";
+
+export function projects(){
+
+    let section = document.createElement('section');
+    section.className = "section";
+
+    let div = document.createElement('div');
+    div.className = "lista-proyectos";
+
+
+    dataProyectos.forEach( (proyecto) => {
+
+        div.appendChild(cadaProject(proyecto.nombre, proyecto.categoria ,  proyecto.githubPage, proyecto.github, proyecto.stacks));
+
+    }); 
+
+    section.appendChild(div);
+
+    return section;
+
+}
